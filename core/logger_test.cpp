5112e0bad964c37002899778c78fe1948ece30cd
Josnelihurt <josnelihurt@gmail.com>
2025-10-08 20:23:14 -0700

    test: add unit tests for core logger

---
+++ core/logger_test.cpp
@@ -0,0 +1,55 @@
+#include "core/logger.h"
+#include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
+
+namespace jrb::core {
+namespace {
+
+class LoggerTest : public ::testing::Test {
+protected:
+    void TearDown() override {
+        // Reset logger after each test
+        spdlog::drop_all();
+        spdlog::set_default_logger(nullptr);
+    }
+};
+
+TEST_F(LoggerTest, InitializeLoggerCreatesDefaultLogger) {
+    // Arrange
+    auto uut = initialize_logger;
+
+    // Act
+    uut();
+
+    // Assert
+    auto default_logger = spdlog::default_logger();
+    ASSERT_NE(default_logger, nullptr);
+    EXPECT_EQ(default_logger->name(), "main");
+}
+
+TEST_F(LoggerTest, InitializeLoggerSetsInfoLevel) {
+    // Arrange
+    auto uut = initialize_logger;
+
+    // Act
+    uut();
+
+    // Assert
+    auto default_logger = spdlog::default_logger();
+    ASSERT_NE(default_logger, nullptr);
+    EXPECT_EQ(spdlog::get_level(), spdlog::level::info);
+}
+
+TEST_F(LoggerTest, LoggerCanBeUsedAfterInitialization) {
+    // Arrange
+    auto uut = initialize_logger;
+    uut();
+
+    // Act & Assert (should not throw)
+    EXPECT_NO_THROW(spdlog::info("Test log message"));
+    EXPECT_NO_THROW(spdlog::debug("Debug message"));
+    EXPECT_NO_THROW(spdlog::error("Error message"));
+}
+
+}  // namespace
+}  // namespace jrb::core

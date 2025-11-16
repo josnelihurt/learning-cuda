4bc75a288b16f65d1aa7f633d5a6607d0bb4b7db
Josnelihurt <josnelihurt@gmail.com>
2025-11-16 12:31:47 -0800

    fix(ci): remove unsupported -Wmissing-requires pragma for ARM64 GCC
    The -Wmissing-requires warning option is not recognized by GCC on ARM64,
    causing build failures. Remove these pragmas as they are not critical
    for the build.
    
    Fixes CI failure in gRPC server image build for ARM64.

--- cpp_accelerator/ports/grpc/processor_engine_provider.h
+++ cpp_accelerator/ports/grpc/processor_engine_provider.h
@@ -4,7 +4,6 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#pragma GCC diagnostic ignored "-Wmissing-requires"
 #include "proto/_virtual_imports/image_processor_service_proto/image_processor_service.pb.h"
 #pragma GCC diagnostic pop
 

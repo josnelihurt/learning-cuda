629414b3688a8e43e56b7cdf882bdfaa123b1f70
Josnelihurt <josnelihurt@gmail.com>
2026-04-13 14:44:04 -0700

    chore(04-01): add manageWebRTC singleton export
    - Exported singleton instance of ReactWebRTCService
    - Provides centralized access point for WebRTC functionality in React app

---
+++ front-end/src/react/infrastructure/connection/webrtc-manage.ts
@@ -0,0 +1,8 @@
+/**
+ * Singleton instance of ReactWebRTCService
+ * Exported for use throughout the React application
+ */
+
+import { ReactWebRTCService } from './ReactWebRTCService';
+
+export const manageWebRTC = new ReactWebRTCService();

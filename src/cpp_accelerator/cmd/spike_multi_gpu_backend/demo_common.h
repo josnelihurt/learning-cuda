9a0a3d31c4a0ed93afe98dd808f859c6ed2a493f
Josnelihurt <josnelihurt@gmail.com>
2026-05-01 12:14:25 -0700

    chore: remove code from pocs

--- src/cpp_accelerator/cmd/spike_multi_gpu_backend/demo_common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "src/cpp_accelerator/cmd/spike_multi_gpu_backend/grayscale_backend.h"
-
-namespace jrb::spike::multi_backend {
-
-int RunGrayscaleDemo(IGrayscaleBackend& backend);
-
-}  // namespace jrb::spike::multi_backend
